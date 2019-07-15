123bee0670d0ceb1a3bb81cfc212d46650ff6d62
CryptoN999 <nicholasgallucci4@gmail.com>
2019-07-14 18:16:48 -0700

    Update prototypes.js

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -34,9 +34,9 @@ CuboidMaker.prototype.surfaceArea = function() {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 const cuboid = new CuboidMaker({
-  length: '4',
-  width: '5',
-  height: '5',
+  length: 4,
+  width: 5,
+  height: 5,
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
